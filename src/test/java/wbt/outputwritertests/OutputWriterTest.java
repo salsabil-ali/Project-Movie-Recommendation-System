1dd49ee0516348bfae44f29054a971c60d3a909d
Nour-Elrweny <nourkhaledmohy@hotmail.com>
2026-05-08 16:41:53 +0300

    Output Writer WBT

--- src/test/java/wbt/outputwritertests/OutputWriterTest.java
+++ src/test/java/wbt/outputwritertests/OutputWriterTest.java
@@ -1,5 +1,42 @@
 package wbt.outputwritertests;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import outputwriter.OutputWriter;
+
+// This test case is the same as the BBT test case.
 public class OutputWriterTest {
-    
+
+    OutputWriter writer;
+
+    @TempDir
+    Path tempDir;
+
+    @BeforeEach
+    void setUp() {
+        writer = new OutputWriter();
+    }
+
+    // Statement coverage: every line in writeRecommendations() executes
+    @Test
+    void writeRecommendations_writesContentToFile() throws Exception {
+        File output = tempDir.resolve("recommendations.txt").toFile();
+        writer.writeRecommendations("some content", output.getAbsolutePath());
+        assertEquals("some content", Files.readString(output.toPath()));
+    }
+
+    // Statement coverage: every line in writeError() executes
+    @Test
+    void writeError_writesErrorMessageToFile() throws Exception {
+        File output = tempDir.resolve("recommendations.txt").toFile();
+        writer.writeError("some error", output.getAbsolutePath());
+        assertEquals("some error", Files.readString(output.toPath()));
+    }
 }
